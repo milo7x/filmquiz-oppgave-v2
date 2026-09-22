0a7ef6b11896df24be1146e44fcc508221b9a94e
Milosz <miloszmika43@gmail.com>
2026-09-22 17:10:42 +0200

    Lagte til de to første filmspørsmålene, opprettet en liste med spørsmål og fire svaralternativer Refs #3

---
+++ script.js
@@ -0,0 +1,12 @@
+const sporsmalListe = [
+  {
+    tekst: "Hva heter cowboyen i Toy Story?",
+    alternativer: ["Woody", "Buzz", "Andy", "Rex"],
+    riktig: 1
+  },
+  {
+    tekst: "Hvilken skole går Harry Potter på?",
+    alternativer: ["Narnia", "Hogwart", "Nevermore", "Durmstrang"],
+    riktig: 2
+  }
+];
